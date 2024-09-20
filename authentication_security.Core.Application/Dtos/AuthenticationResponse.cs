5aaf0891d3c83dd0b52c59c522a7500d5f4c5f6c
Russel Martinez <russel_martinez@bhd.com.do>
2024-09-20 16:44:07 -0400

    Done

---
+++ authentication_security.Core.Application/Dtos/AuthenticationResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace authentication_security.Core.Application.Dtos
+{
+    internal class AuthenticationResponse
+    {
+    }
+}
